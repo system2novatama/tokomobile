0f65c2dd6509bee00a8483bb04b252fe0509b481
system2novatama <system2@novatama.co.id>
2015-02-16 20:23:46 +0700

    1

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-	var shop_name = "KYOKO-YOKO"; // NAMA TOKO ONLINE
-	var domain = "http://kyoko-yoko.toko-mobile.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = " PDF Grosir"; // NAMA TOKO ONLINE
+	var domain = "http://primadewatafashion.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/kyokoyoko"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "b0b74d0fedba3d453514b0dc64b22016"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/PrimaDewataFashion"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "7e2f4c3e5737f2799ddda918cb67da13"; // ISI DENGAN TOKEN 
 	
 	var cart_item_id = new Array();
 	var cart_item_qty = new Array();
