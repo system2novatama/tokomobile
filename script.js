484f60beb003444a0a7181329214e566da02b714
system2novatama <system2@novatama.co.id>
2015-03-04 14:44:07 +0700

    1

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-	var shop_name = "Giselle Fashion"; // NAMA TOKO ONLINE
-	var domain = "http://gisellefashion.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Kusuma Grosir"; // NAMA TOKO ONLINE
+	var domain = "http://kusumagrosir.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/GiselleFashion"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "45b2e781c32d18cf713beb2d70030b53"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/kusumagrosir"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "47b8c837f49a5e84f804d6d8b875ffaa"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
