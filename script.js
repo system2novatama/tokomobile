ed40d70ad7b248cbeeb222bf5d562e1250cecb51
system2novatama <system2@novatama.co.id>
2015-03-04 15:59:10 +0700

    1

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-	var shop_name = "Mimiponishop"; // NAMA TOKO ONLINE
-	var domain = "http://mimiponishop.toko-mobile.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Clarisshop 789"; // NAMA TOKO ONLINE
+	var domain = "http://clarisshop789.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/Mimiponishop"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "f363815e9ccce06ccbbac332ca1df426"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/Clarisshop789"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "f77285dbee15280e97aa2274e9f9164e"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
