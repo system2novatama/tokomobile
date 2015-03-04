9d124fafe628204daee31d5e055487149fa65707
system2novatama <system2@novatama.co.id>
2015-03-04 14:40:25 +0700

    11

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-	var shop_name = "Gelarez Shop"; // NAMA TOKO ONLINE
-	var domain = "http://gelarezshop.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Giselle Fashion"; // NAMA TOKO ONLINE
+	var domain = "http://gisellefashion.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/gelarezshop"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "83dd0ddcea8668d77b2eb7d382efc6dd"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/GiselleFashion"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "45b2e781c32d18cf713beb2d70030b53"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
