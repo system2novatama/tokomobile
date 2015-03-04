c320c84c8ce79338fb2737258d46ce5d10314450
system2novatama <system2@novatama.co.id>
2015-03-04 12:47:48 +0700

    1

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-	var shop_name = "Kusuma Grosir"; // NAMA TOKO ONLINE
-	var domain = "http://kusumagrosir.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Gelarez Shop"; // NAMA TOKO ONLINE
+	var domain = "http://gelarezshop.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/kusumagrosir"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "47b8c837f49a5e84f804d6d8b875ffaa"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/gelarezshop"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = " 83dd0ddcea8668d77b2eb7d382efc6dd"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
