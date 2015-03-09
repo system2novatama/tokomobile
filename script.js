9d74fdc07f10fc89f769b8816cd3dec1a024e7c1
system2novatama <system2@novatama.co.id>
2015-03-09 15:46:52 +0700

    1

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-	var shop_name = "Moonpinkshop"; // NAMA TOKO ONLINE
-	var domain = "http://moonpinkshop.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "NAF Butik"; // NAMA TOKO ONLINE
+	var domain = "http://naf-butik.toko-mobile.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/moonpinkshop"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "0cbb8222e82759eefb65308238d4cb0c"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/NAFButik"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "60caa41a23d8f85f005e58b8c2265506"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
