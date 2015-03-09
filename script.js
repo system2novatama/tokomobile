97c0710bd299fd07317f68383f2a526ddec15d9f
system2novatama <system2@novatama.co.id>
2015-03-09 15:57:02 +0700

    1

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-	var shop_name = "NAF Butik"; // NAMA TOKO ONLINE
-	var domain = "http://naf-butik.toko-mobile.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "OKIK OS"; // NAMA TOKO ONLINE
+	var domain = "http://okikos.net/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/NAFButik"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "60caa41a23d8f85f005e58b8c2265506"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/okikos"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "22c11046accd5293755f08c7adb8ae3c"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
