c3c60e327443e99c9da286def3721479c0dee2c4
system2novatama <system2@novatama.co.id>
2015-03-10 11:16:25 +0700

    1

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-	var shop_name = "F2 Fashion"; // NAMA TOKO ONLINE
-	var domain = "http://f2-fashion.toko-mobile.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Thiors OLS"; // NAMA TOKO ONLINE
+	var domain = "http://thiors-ols.toko-mobile.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/f2fashion"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "0f264b0f53d7835095c9a29e036d964e"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/thios"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "f7ef7d8852909950e24f0c1d78210074"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
