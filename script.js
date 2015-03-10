46bc3bf7e9d0b7c9e9d706c1fff3e36a5d9a5117
system2novatama <system2@novatama.co.id>
2015-03-10 13:45:23 +0700

    1

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-	var shop_name = "Mimiponishop"; // NAMA TOKO ONLINE
-	var domain = "http://mimiponishop.toko-mobile.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Wiyan Shop"; // NAMA TOKO ONLINE
+	var domain = "http://wiyanshop.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/Mimiponishop"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "f363815e9ccce06ccbbac332ca1df426"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/WiyanShop"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "f2f865b123149f1bb50d90cd7081fc3f"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
