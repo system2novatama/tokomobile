d36e50f5dd69a19c2b67a38a0dd372196ca4320a
system2novatama <system2@novatama.co.id>
2015-03-12 16:09:44 +0700

    1

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-	var shop_name = "Tokomobile Development"; // NAMA TOKO ONLINE
-	var domain = "http://192.168.1.200/"; // DOMAIN URL ADMIN
+	var shop_name = "Dekasun"; // NAMA TOKO ONLINE
+	var domain = "http://dekasun.toko-mobile.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/Tokomobile"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "6aedc689df342b934b6e8f82a18838af"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/Dekasun"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "04d6d8b152c8edaa82f8abdef47737e8"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
