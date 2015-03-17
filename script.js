c35c106eeb87d730a6d06227ceeb69167eb45de0
system2novatama <system2@novatama.co.id>
2015-03-17 17:13:32 +0700

    1

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-	var shop_name = "SF Fashion"; // NAMA TOKO ONLINE
-	var domain = "http://sf-fashiongrosir.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Vendow"; // NAMA TOKO ONLINE
+	var domain = "http://vendowonlineshop.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/SFFashion"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "04aa3e0a065fbe086998bfbc69d4f8df"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/Vendow"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "f7dd92639ed7ace8e4dbdad5f95144e2"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
