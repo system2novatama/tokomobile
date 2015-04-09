09f830a812f02c615100a9f267759884e8d981c8
system2 <system2@novatama.co.id>
2015-04-09 14:21:44 +0700

    repo

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-	var shop_name = "ABZA Collection"; // NAMA TOKO ONLINE
-	var domain = "http://abzacollection.linkaplikasi.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Bello"; // NAMA TOKO ONLINE
+	var domain = "http://bellofashion.net/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/ABZAcollection"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "34263ac7dc497fd091f4df8518a5e144"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/Bello"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "8de493c8cf14e41dc081c3494a4e2be5"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
