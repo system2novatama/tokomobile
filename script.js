663c1b7d3d2f1f5e853a378a389da6b8dbce12b9
system2 <system2@novatama.co.id>
2015-04-09 16:18:28 +0700

    test

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-	var shop_name = "GLAMZ"; // NAMA TOKO ONLINE
-	var domain = "http://glamzshop.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Yutakabee"; // NAMA TOKO ONLINE
+	var domain = "http://yutakabee.net/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/GLAMZ"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "c31acc103a16d487c2b6ae79d95dd91e"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/Yutakabee"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "92e12884a66082189ed49ec9d0e8eabb"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
