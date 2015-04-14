2730e81bd5d30264711a9268590e64e35e14c064
system2 <system2@novatama.co.id>
2015-04-14 20:26:09 +0700

    adaw

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-	var shop_name = "Quete Online Shop"; // NAMA TOKO ONLINE
-	var domain = "http://queteonlineshop.linkaplikasi.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Owls House"; // NAMA TOKO ONLINE
+	var domain = "http://owls-house.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/QueteOnlineShop"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "b68698c301eda8daddeb05c98bbf82c7"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/OwlsHouse"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "56269f88b85f6fb6e3b2803f0ac09f65"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
