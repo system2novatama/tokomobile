848179b396173e00dec9ecda9015cc359f8dcd79
system2novatama <benq@benq>
2015-04-20 13:21:56 +0700

    aaa

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-	var shop_name = "PGBO"; // NAMA TOKO ONLINE
-	var domain = "http://pgbo.linkaplikasi.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Qeute Online Shop"; // NAMA TOKO ONLINE
+	var domain = "http://qeuteonlineshop.linkaplikasi.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/PGBO"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "4a921145231f076d5dbeccebc9d57e24"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/QeuteOnlineShop"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "a18fd213cbc2663bc48c2a6fecebc512"; // ISI DENGAN TOKEN 
 	
 	var cart_item_id = new Array();
 	var cart_item_qty = new Array();
