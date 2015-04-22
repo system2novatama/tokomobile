637ceadcf951dbd6cd13899c8e9a70011da5b339
system2novatama <benq@benq>
2015-04-22 12:46:55 +0700

    aa

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-	var shop_name = "QNK Fashion"; // NAMA TOKO ONLINE
-	var domain = "http://qnkfashion.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "PGBO"; // NAMA TOKO ONLINE
+	var domain = "http://pgbo.linkaplikasi.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/QNK-Fashion"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "4f48836fdc7952bb27d7663649fff4eb"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/PGBO"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "4a921145231f076d5dbeccebc9d57e24"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
