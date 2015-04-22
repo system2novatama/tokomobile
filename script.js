ea69fe4df9356c62b1606ff2a5760167a3ef9f35
system2novatama <benq@benq>
2015-04-22 12:55:03 +0700

    aa

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-	var shop_name = "TwentyoneshopID"; // NAMA TOKO ONLINE
-	var domain = "http://twentyonemall.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Kim Baby Shop"; // NAMA TOKO ONLINE
+	var domain = "http://kimbabyshop.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/TwentyoneshopID"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "1a986a371c706ac65ab0a6122fbb71a7"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/KimBabyShop"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "318907a774a053206a72bdc0f197cd6a"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
