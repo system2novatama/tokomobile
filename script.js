b31bb34c1b240395e2286eff46a95384f1a41524
system2novatama <benq@benq>
2015-04-23 18:37:05 +0700

    aaa

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-	var shop_name = "WeShip.id"; // NAMA TOKO ONLINE
-	var domain = "http://admin.weship.id/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Dosti Butik"; // NAMA TOKO ONLINE
+	var domain = "http://dostibutik.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/WeShip"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "850e4511f2a87b82da2fff6dcda8c40d"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/DostiButik"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "349d9dda326b83c300efae82a16e9883"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
