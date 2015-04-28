be021c2c0cc4c901346fa76f2f3d58af07c59032
system2novatama <benq@benq>
2015-04-28 13:37:15 +0700

    aaa

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-	var shop_name = "FK Hijab"; // NAMA TOKO ONLINE
-	var domain = "http://fkhijab.linkaplikasi.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Femme"; // NAMA TOKO ONLINE
+	var domain = "http://femmeolshop.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/FKHijab"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "1401d071b5b65dd0241def7744a1f240"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/Femme"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "db6d0d2b3bd5250b5a613eef059a4391"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
