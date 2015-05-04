9fd5ef78b33a9d26ef9afe178ad3a684fec98c4f
system2novatama <benq@benq>
2015-05-04 14:23:10 +0700

    aaa

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-	var shop_name = "Radista"; // NAMA TOKO ONLINE
-	var domain = "http://radistashop.net/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Kenzie Fashion"; // NAMA TOKO ONLINE
+	var domain = "http://kenziefashion.linkaplikasi.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/Radista"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "2f5fc7bff135281087124896cc207041"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/kenziefashion"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "aa46f6b312f942dfd7609a920fdec1c0"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
