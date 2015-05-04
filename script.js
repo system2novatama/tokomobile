ed0a58a717e216a55d86aa79445a31d61e2bb25c
system2novatama <benq@benq>
2015-05-04 15:02:48 +0700

    aaa

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-	var shop_name = "Kenzie Fashion"; // NAMA TOKO ONLINE
-	var domain = "http://kenziefashion.linkaplikasi.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "RFS Clothing"; // NAMA TOKO ONLINE
+	var domain = "http://rfsclothing.linkaplikasi.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/kenziefashion"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "aa46f6b312f942dfd7609a920fdec1c0"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/rfsclothing"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "1519cb44f7ae7aafcaac8556c65bd33c"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
