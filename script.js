d4d8389840fe26768e9d32bb98b15a464dfb73b4
system2novatama <benq@benq>
2015-05-11 17:38:10 +0700

    aaa

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-	var shop_name = "FOS"; // NAMA TOKO ONLINE
-	var domain = "http://foscollection.com/mobile/"; // DOMAIN URL ADMIN
+	var shop_name = "Hermosa Fashion"; // NAMA TOKO ONLINE
+	var domain = "http://hermosa-fashion.com/mobile/"; // DOMAIN URL ADMIN
 	var admin_url = domain;
 	
 	var base_url = domain+"_api_/android"; // URL API
 	var base_url_media = admin_url+"media"; // DIREKTORI PENYIMPANAN IMAGE DI HOSTING
-	var dir_image = "Pictures/foscollection"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
-	var token = "8447c39e5db61d7617c322bfec90277f"; // ISI DENGAN TOKEN 
+	var dir_image = "Pictures/hermosafashion"; // DIREKTORI PENYIMPANAN IMAGE DI SD CARD
+	var token = "03e893d2c8e2f743bcda673d4ed25c3c"; // ISI DENGAN TOKEN 
 	
 var cart_item_id = new Array();
 	var cart_item_qty = new Array();
